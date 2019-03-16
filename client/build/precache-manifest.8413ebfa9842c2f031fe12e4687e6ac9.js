67b2161234f9b8615b48821c3d09d58e63beaf76
Adam Szaruga <adamszaruga@gmail.com>
2019-03-16 12:31:34 -0400

    build folder

---
+++ client/build/precache-manifest.8413ebfa9842c2f031fe12e4687e6ac9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "ea3ccad6d33e7e43b598",
+    "url": "/static/js/main.17be5890.chunk.js"
+  },
+  {
+    "revision": "ba264cd90c4d36546d5a",
+    "url": "/static/js/2.8f71a45e.chunk.js"
+  },
+  {
+    "revision": "ea3ccad6d33e7e43b598",
+    "url": "/static/css/main.1a06b216.chunk.css"
+  },
+  {
+    "revision": "5367cb852b758449420226a9d0a51a1d",
+    "url": "/index.html"
+  }
+];
